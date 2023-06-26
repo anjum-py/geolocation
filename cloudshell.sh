e8abbd611f77ba5ed2cdf7be5903e898ba4bd66b
Anjum <baleeguddin@gmail.com>
2023-06-26 10:30:19 +0530

    Refactor cloud shell

--- cloudshell.sh
+++ cloudshell.sh
@@ -22,6 +22,10 @@ add_line_to_bashrc() {
 
 # Function to install pyenv and set Python version
 install_pyenv_and_python() {
+    add_line_to_bashrc 'export PYENV_ROOT="$HOME/.pyenv"'
+    add_line_to_bashrc 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
+    source ~/.bashrc
+
     # Check if pyenv is installed
     if command -v pyenv >/dev/null 2>&1; then
         echo "---------------------------------------------------------"
@@ -32,15 +36,13 @@ install_pyenv_and_python() {
         curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
         # Update ~/.bashrc with pyenv configuration if lines don't exist
-        add_line_to_bashrc 'export PYENV_ROOT="$HOME/.pyenv"'
-        add_line_to_bashrc 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
         add_line_to_bashrc 'eval "$(pyenv init -)"'
-
+        source ~/.bashrc
         # Reload the shell
         echo "---------------------------------------------------------"
         echo "pyenv installed. Checking Python version..."
     fi
-    source ~/.bashrc
+    
     # Check if Python version is already greater than or equal to 3.11.0
     if python3 -c "import sys; exit(0) if sys.version_info >= (3, 11, 4) else exit(1)"; then
         echo "---------------------------------------------------------"
